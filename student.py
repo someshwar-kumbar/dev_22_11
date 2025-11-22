54a334f1bec7e5ff8d8737bb1b47c2a8e4df718d
someshwar-kumbar <tncxsomu@gmail.com>
2025-11-22 10:31:52 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,12 +1,13 @@
 import sys
+
 if len(sys.argv) != 3:
-  print("usage : python student.py <name>,<rollno>")
-  sys.exit(1)
+    print("usage : python student.py <name> <rollno>")
+    sys.exit(1)
 
-script_name=sys.argv[0]
-name=sys.argv[1]
-rollno=sys.arge[2]
+script_name = sys.argv[0]
+name = sys.argv[1]
+rollno = sys.argv[2]   # corrected typo: sys.arge → sys.argv
 
-print("script name :",script_name)
-print("student name :", name )
-print("roll number ",rollno)
+print("script name :", script_name)
+print("student name :", name)
+print("roll number :", rollno)
